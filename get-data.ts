9a6dac41b926612a2bf70a546670e5512376057d
Nana Elie <91574567+nanaelie@users.noreply.github.com>
2025-05-16 04:47:22 +0000

    fix: correct getData call by using proper named parameters

--- get-data.ts
+++ get-data.ts
@@ -1,33 +1,42 @@
 import Data from './data';
 
 interface ReqProps {
-	prix?: string;
-	hippo?: string;
-	date?: string;
-};
+  prix?: string;
+  hippo?: string;
+  date?: string;
+}
 
 type ArriveeType = {
-	prix: string;
-	hippodrome: string;
-	date: string;
-	arrivee: string;
+  prix: string;
+  hippodrome: string;
+  date: string;
+  arrivee: string;
 };
 
 const data: ArriveeType[] = Data;
 
-const getData = ({ prix, hippo, date }: ReqProps): ArriveeType[] => {
-	let query: string | undefined;
-	let resp: ArriveeType[];
+const getData = ({ prix, hippo, date }: ReqProps): [boolean, ArriveeType[] | string] => {
+  let resp: ArriveeType[] = [];
 
-	if (prix) {
-		resp = data.filter(arrivee => arrivee.prix.toLowerCase().includes(prix.toLowerCase()));
-	} else if (hippo) {
-		resp = data.filter(arrivee => arrivee.hippodrome.toLowerCase().includes(hippo.toLowerCase()));
-	} else if (date) {
-		resp = data.filter(arrivee => arrivee.date.toLowerCase().includes(date.toLowerCase()));
-	}
+  if (prix) {
+    resp = data.filter(arrivee =>
+      arrivee.prix.toLowerCase().includes(prix.toLowerCase())
+    );
+  } else if (hippo) {
+    resp = data.filter(arrivee =>
+      arrivee.hippodrome.toLowerCase().includes(hippo.toLowerCase())
+    );
+  } else if (date) {
+    resp = data.filter(arrivee =>
+      arrivee.date.toLowerCase().includes(date.toLowerCase())
+    );
+  }
 
-	return resp;
-}
+  if (resp.length > 0) {
+    return [true, resp];
+  }
+
+  return [false, "Pas de correspondance"];
+};
 
 export default getData;
