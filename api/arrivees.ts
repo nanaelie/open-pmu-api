f49db2f40ff52c74dc555f35e67bfadf10632aed
Nana Elie <91574567+nanaelie@users.noreply.github.com>
2025-05-16 04:43:19 +0000

    fix: Fixing a mistake

--- api/arrivees.ts
+++ api/arrivees.ts
@@ -1,36 +1,43 @@
 import getData from '../get-data';
 
-module.exports = async (req, res) => {
-	if (req.method !== 'GET') {
-		res.status(405).send('Method Not Allowed');
-		return;
-	}
-
-	try {
-		let prix = req.query.prix;
-		let hippo = req.query.prix;
-		let date = req.query.date;
-		let resp: string | undefined;
-
-		if (prix !== "") {
-			const resp = getData({ prix: prix });    
-		}else if (hippo !== "") {
-			const resp = getData({ hippo: hippo });    
-		}else if (date !== "") {
-			const resp = getData({ date: date });    
-		}else {
-			const resp = {
-				"error": true, 
-				"message": "Aucun paramètre fourni."
-			};
-		}
-
-		res.setHeader('Content-Type', 'application/json');
-		console.log(resp);
-		res.send(resp);
-
-	} catch (error) {
-		res.status(500).send("Erreur serveur.");
-	}
+type ArriveeType = {
+  prix: string;
+  hippodrome: string;
+  date: string;
+  arrivee: string;
 };
 
+module.exports = (req, res) => {
+  if (req.method !== 'GET') {
+    res.status(405).send('Method Not Allowed');
+    return;
+  }
+
+  try {
+    const prix = req.query.prix as string;
+    const hippo = req.query.hippo as string;
+    const date = req.query.date as string;
+
+    let _resp: [boolean, ArriveeType[] | string];
+
+    if (prix) {
+      _resp = getData({ prix });
+    } else if (hippo) {
+      _resp = getData({ hippo });
+    } else if (date) {
+      _resp = getData({ date });
+    } else {
+      _resp = [false, "Aucun paramètre fourni."];
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send({
+      error: !_resp[0],
+      message: _resp[1],
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erreur serveur.");
+  }
+};
