db0621e162ff0b6144ebcf0976a227afadf02e71
Nana Elie <elienana92@gmail.com>
2025-12-23 17:54:34 +0000

    Updated file  get-data.js

--- get-data.js
+++ get-data.js
@@ -1,23 +1,56 @@
-"use strict";
+/*"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var data_1 = require("./data");
-;
-var data = data_1.default;
-var getData = function (_a) {
-    var prix = _a.prix, hippo = _a.hippo, date = _a.date;
-    var query;
-    var resp;
+const racing_horse_data_1 = __importDefault(require("./racing-horse-data"));
+const data = racing_horse_data_1.default;
+const getData = ({ prix, hippo, date }) => {
+    let resp = [];
     if (prix) {
-        resp = data.filter(function (arrivee) { return arrivee.prix.toLowerCase().includes(prix.toLowerCase()); });
+        resp = data.filter(arrivee => arrivee.prix.toLowerCase().includes(prix.toLowerCase()));
     }
     else if (hippo) {
-        resp = data.filter(function (arrivee) { return arrivee.hippodrome.toLowerCase().includes(hippo.toLowerCase()); });
+        resp = data.filter(arrivee => arrivee.lieu.toLowerCase().includes(hippo.toLowerCase()));
     }
     else if (date) {
-        resp = data.filter(function (arrivee) { return arrivee.date.toLowerCase().includes(date.toLowerCase()); });
+        resp = data.filter(arrivee => arrivee.date.toLowerCase().includes(date.toLowerCase()));
     }
-    console.log('Donnée: ', resp);
-    return resp;
+    if (resp.length > 0) {
+        return [true, resp];
+    }
+    return [false, "Pas de correspondance"];
 };
-getData({ date: "05/05/2021" });
 exports.default = getData;
+
+*/
+
+import Data from './racing-horse-data';
+
+const data = Data;
+
+const getData = ({ prix, hippo, date }) => {
+  let resp = [];
+
+  if (prix) {
+    resp = data.filter(arrivee =>
+      arrivee.prix.toLowerCase().includes(prix.toLowerCase())
+    );
+  } else if (hippo) {
+    resp = data.filter(arrivee =>
+      arrivee.lieu.toLowerCase().includes(hippo.toLowerCase())
+    );
+  } else if (date) {
+    resp = data.filter(arrivee =>
+      arrivee.date.toLowerCase().includes(date.toLowerCase())
+    );
+  }
+
+  if (resp.length > 0) {
+    return [true, resp];
+  }
+
+  return [false, "Pas de correspondance"];
+};
+
+export default getData;
