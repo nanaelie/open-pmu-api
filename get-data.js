4b739149f1bda66d2c2e7aee76b7210cbaad8caf
Nana Elie <elienana92@gmail.com>
2026-07-14 21:18:27 +0000

    chore: touch get-data.js

--- get-data.js
+++ get-data.js
@@ -1,56 +1,111 @@
-/*"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const racing_horse_data_1 = __importDefault(require("./racing-horse-data"));
-const data = racing_horse_data_1.default;
-const getData = ({ prix, hippo, date }) => {
-    let resp = [];
-    if (prix) {
-        resp = data.filter(arrivee => arrivee.prix.toLowerCase().includes(prix.toLowerCase()));
-    }
-    else if (hippo) {
-        resp = data.filter(arrivee => arrivee.lieu.toLowerCase().includes(hippo.toLowerCase()));
-    }
-    else if (date) {
-        resp = data.filter(arrivee => arrivee.date.toLowerCase().includes(date.toLowerCase()));
-    }
-    if (resp.length > 0) {
-        return [true, resp];
-    }
-    return [false, "Pas de correspondance"];
-};
-exports.default = getData;
+import db from './connections/index.js';
 
-*/
+export default async function getData({ prix, hippo, date }) {
+	try {
+		let key = null, value = null, operator = 'LIKE';
+		if (prix) {
+			key = 'races.prix';
+			value = prix;
+		} else if (hippo) {
+			key = 'races.lieu';
+			value = hippo;
+		} else if (date) {
+			key = 'races.date';
+			const formattedDate = new Date(date).toISOString().split("T")[0];
+			value = formattedDate;
+			operator = '='
+		}
 
-import Data from './racing-horse-data.js';
+		if (key && value) {
+			const result = await db.query(`
+				SELECT 
+					races.id,
+					races.type,
+					races.date,
+					races.montant,
+					races.distance,
+					races.prix,
+					races.lieu,
+					races.heure_depart,
+					races.details,
+					races.partants,
+					races.non_partants,
+					races.arrivee,
+					races.r_c,
+		
+					race_results.nom_cheval,
+					race_results.sexe,
+					race_results.annee_de_naissance,
+					race_results.nom_jockey,
+					race_results.nom_entraineur,
+					race_results.poids_cheval,
+					race_results.musique,
+					race_results.cotes,
+					race_results.gains,
+					race_results.corde,
+					race_results.discipline,
+					race_results.distance AS horse_distance,
+					race_results.numero
+		
+				FROM races
+				LEFT JOIN race_results 
+					ON race_results.race_id = races.id
+		
+				WHERE ${key} ${operator} $1
+			`, [operator == 'LIKE' ? `%${value}%` : date]
+			);
+			const races = {};
+	
+			for (const row of result.rows) {
+	
+				if (!races[row.id]) {
+					races[row.id] = {
+						type: row.type,
+						montant: row.montant,
+						distance: row.distance,
+						prix: row.prix,
+						lieu: row.lieu,
+						heure_depart: row.heure_depart,
+						details: row.details,
+						partants: row.partants,
+						non_partants: row.non_partants,
+						arrivee: row.arrivee,
+						"r/c": row.r_c,
+						arrivee_details: {},
+						date: row.date
+					};
+				}
+	
+				if (row.numero !== null) {
+					races[row.id].arrivee_details[row.numero] = {
+						nom_cheval: row.nom_cheval,
+						sexe: row.sexe,
+						annee_de_naissance: row.annee_de_naissance,
+						nom_jockey: row.nom_jockey,
+						nom_entraineur: row.nom_entraineur,
+						poid_du_cheval: row.poids_cheval,
+						musique: row.musique,
+						cotes: row.cotes,
+						gains: row.gains,
+						corde: row.corde,
+						discipline: row.discipline,
+						distance: row.horse_distance
+					};
+				}
+			}
+	
+			const response = Object.values(races);
+			if (response.length > 0) {
+				return [true, response];
+			} else {
+				return [false, "Pas de correspondance"];
+			}
+		} else {
+			return [false, "Pas de correspondance"];
+		}
+	} catch (err) {
+		console.log(err);
+		return [false, []]
+	}
+}
 
-const data = Data;
-
-const getData = ({ prix, hippo, date }) => {
-  let resp = [];
-
-  if (prix) {
-    resp = data.filter(arrivee =>
-      arrivee.prix.toLowerCase().includes(prix.toLowerCase())
-    );
-  } else if (hippo) {
-    resp = data.filter(arrivee =>
-      arrivee.lieu.toLowerCase().includes(hippo.toLowerCase())
-    );
-  } else if (date) {
-    resp = data.filter(arrivee =>
-      arrivee.date.toLowerCase().includes(date.toLowerCase())
-    );
-  }
-
-  if (resp.length > 0) {
-    return [true, resp];
-  }
-
-  return [false, "Pas de correspondance"];
-};
-
-export default getData;
